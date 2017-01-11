ac321da396699ee6710fdcd36e7ba400ee523df2
Ankit Gyawali <dev@ankitgyawali.com>
2017-01-11 11:55:37 -0500

    remove test files

--- api/hellworld.js
+++
@@ -1,9 +0,0 @@
-// hello-package/api/helloworld.js
-var helloworld = module.exports;
-function hello(req,res,next) {
-    console.log(res);
-    res.send('Hello world!');
-}
- helloworld.routes = [
-    { path: '/hello', httpMethod: 'GET', middleware: hello }
-]
