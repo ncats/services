9dcf4235af6f9b4de9c90e020b94308f9ebaf8bb
KalleV <kvirtaneva@gmail.com>
2017-03-29 11:05:03 -0600

    Update /version, /versions, and /endpoint APIs
     - Rename /version to /versions when listing all loaded LabShare service
    metadata
     - Update tests
     - Simplify array iteration using forEach and lodash
     - Fix bug where an undefined route would be loaded by the ApiLoader
    when running LabShare Services as a standalone service via `lsc services start`

--- api/api-config.js
+++ api/api-config.js
@@ -1,18 +1,109 @@
 'use strict';
 
-const version = require('./version'),
-    endpoints = require('./endpoints'),
-    _ = require('lodash');
+const _ = require('lodash');
 
-exports.config = function (data) {
-    const {apiLoader, app} = data;
-    const versionDependencies = [];
-    for (let key in apiLoader.services) {
-        if (apiLoader.services.hasOwnProperty(key) && !_.isEmpty(apiLoader.services[key])) {
-                versionDependencies.push(version.versionDependencies(apiLoader.manifest[key], key));
-                apiLoader.services[key] = _.concat(apiLoader.services[key],endpoints.exposeEndPoint(apiLoader.services[key], key));
+/**
+ * @param versionDependencies An apps dependencies and information gathered from their package.json
+ * @description Parses package.json to retrieve relevant metadata
+ * @returns {Object} Route /version and its information
+ */
+function addVersionRoutes(versionDependencies) {
+
+    function returnVersion(req, res) {
+        res.json({
+            versions: versionDependencies
+        });
+    }
+
+    function returnDependencies(req, res) {
+        let name = req.params.name;
+
+        if (!_.find(versionDependencies, {api: name})) {
+            res.sendStatus(404);
+        } else {
+            res.json({
+                version: _.find(versionDependencies, {api: name})
+            });
         }
     }
-    
-    apiLoader.services[_.head(_.head([versionDependencies])).api] = _.concat((apiLoader.services[_.head(_.head([versionDependencies])).api]), version.addVersionRoutes(versionDependencies, _.head(_.head([versionDependencies])).api));
+
+    return [
+        {
+            path: `/versions`,
+            httpMethod: 'GET',
+            middleware: [returnVersion]
+        },
+        {
+            path: `/:name/version`,
+            httpMethod: 'GET',
+            middleware: [returnDependencies]
+        }
+    ];
+}
+
+
+function getMetadata(manifest, key) {
+    return {
+        api: key,
+        apiDetails: {name: manifest.name, version: manifest.version, description: manifest.description}
+    };
+}
+
+/**
+ * @param {Array} routes List of routes exposed for a particular LabShare service
+ * @param key Name of the service
+ * @description Exposes GET /endpoints to render api as table and POST /endpoints to return table as json
+ * @returns {Object} Route /endpoints and its information
+ */
+function exposeEndPoints(routes, key) {
+    let endPointTable = "<table border='2' cellpadding='20'><thead><tr><td>#</td><td>HTTP Method</td><td>Path</td></thead><tbody>";
+
+    routes.forEach((endpoint, index) => {
+        if (!endpoint) {
+            return;
+        }
+        // console.log(endpoint, index);
+        endPointTable += `<tr><td>${index}</td><td>${endpoint.httpMethod}</td><td>${endpoint.path}</td></tr>`;
+    });
+
+    endPointTable += '</tbody></table>';
+
+    function getReturnEndpoints(req, res) {
+        res.send(endPointTable);
+    }
+
+    function postReturnEndpoints(req, res) {
+        res.json(routes);
+    }
+
+    return [
+        {
+            path: `/${key}/endpoints`,
+            httpMethod: 'GET',
+            middleware: [getReturnEndpoints]
+        },
+        {
+            path: `/${key}/endpoints`,
+            httpMethod: 'POST',
+            middleware: [postReturnEndpoints]
+        }
+    ];
+}
+
+exports.config = function (data) {
+    const {apiLoader} = data;
+
+    let versionDependencies = _.map(apiLoader.manifest, (manifest, key) => {
+        return getMetadata(manifest, key);
+    });
+
+    apiLoader.services['services'] = _.concat(apiLoader.services['services'] || [], addVersionRoutes(versionDependencies));
+
+    _.each(apiLoader.services, (routes, key) => {
+        if (_.isEmpty(routes)) {
+            return;
+        }
+
+        apiLoader.services[key] = _.concat(routes, exposeEndPoints(routes, key));
+    });
 };
