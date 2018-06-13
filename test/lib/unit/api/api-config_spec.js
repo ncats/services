beead4e7af4c57cccf563e7345075e93be044285
KalleV <kvirtaneva@gmail.com>
2018-06-13 16:47:12 -0600

    feat(api): remove recursive dependency resolution SHELL-1402
    BREAKING CHANGE: Remove recursive “packageDependency” resolution for HTTP and Web Socket APIs to resolve Node module versioning conflicts when multiple LabShare versions of LabShare dependencies exist in the dependency tree.

--- test/lib/unit/api/api-config_spec.js
+++ test/lib/unit/api/api-config_spec.js
@@ -1,27 +1,22 @@
 'use strict';
 
-const path = require('path'),
-    supertest = require('supertest'),
-    express = require('express'),
-    {Router} = express;
+const supertest = require('supertest');
+const express = require('express');
+const {Router} = express;
+const ApiLoader = require('../../../../lib/api/loader');
+const apiConfig = require('../../../../api/api-config');
 
 describe('ApiConfig', () => {
-    let ApiLoader,
-        apiLoader,
+
+    let apiLoader,
         expressApp,
         router,
-        request,
-        apiPackage1Prefix,
-        apiConfig;
+        request;
 
     beforeEach(() => {
-        apiPackage1Prefix = '/api-package-1-namespace';
-
         expressApp = express();
         router = Router();
 
-        ApiLoader = require('../../../../lib/api/loader');
-        apiConfig = require('../../../../api/api-config');
         apiLoader = new ApiLoader(router, {
             logger: {error: jasmine.createSpy('error')},
             main: './test/fixtures/main-package',
@@ -40,77 +35,65 @@ describe('ApiConfig', () => {
 
     describe('GET/POST /endpoints', () => {
 
-        it(`gets all the API endpoints associated with the given LabShare package name`, done => {
-            request.get(`/api-package-1-namespace/endpoints`)
-                .expect(200)
-                .then(res => {
-                    expect(res.text).not.toBe(null);
-                    done();
-                })
-                .catch(done.fail);
+        it(`gets all the API endpoints associated with the given LabShare package name`, async () => {
+            const res = await request.get(`/api-package-1-namespace/endpoints`);
+
+            expect(res.statusCode).toBe(200);
+            expect(res.text).not.toBe(null);
         });
 
-        it(`gets all the API endpoints associated with the given LabShare package name`, done => {
-            request.post(`/api-package-1-namespace/endpoints`)
-                .expect(200)
-                .then(res => {
-                    expect(res.body[0].path).toBe('/api-package-1-namespace/:param/_api/hello');
-                    expect(res.body[0].httpMethod).toBe('GET');
-                    expect(res.body[1].path).toBe('/api-package-1-namespace/:param/_api/settings');
-                    expect(res.body[1].httpMethod).toBe('POST');
-                    done();
-                })
-                .catch(done.fail);
+        it(`gets all the API endpoints associated with the given LabShare package name`, async () => {
+            const res = await request.post(`/api-package-1-namespace/endpoints`)
+                .expect(200);
+
+            expect(res.body[0].path).toBe('/api-package-1-namespace/:param/_api/hello');
+            expect(res.body[0].httpMethod).toBe('GET');
+            expect(res.body[1].path).toBe('/api-package-1-namespace/:param/_api/settings');
+            expect(res.body[1].httpMethod).toBe('POST');
         });
 
     });
 
     describe('GET /versions and GET /:name/version', () => {
 
-        it(`gets the package metadata of all LabShare packages that expose HTTP routes`, done => {
-            request.get('/versions')
-                .expect(200)
-                .then(res => {
-                    expect(res.body.versions).toBeDefined();
+        it(`gets the package metadata of all LabShare packages that expose HTTP routes`, async () => {
+            const res = await request.get('/versions')
+                .expect(200);
 
-                    let versions = res.body.versions;
+            expect(res.body.versions).toBeDefined();
 
-                    expect(versions).toEqual(jasmine.arrayContaining([
-                        {
-                            api: 'api-package-1-namespace',
-                            apiDetails: {
-                                name: 'api-package-1',
-                                version: '0.0.1',
-                                description: 'Api package name space'
-                            }
-                        },
-                        {
-                            api: 'api-package-2',
-                            apiDetails: {
-                                name: 'api-package-2',
-                                version: '0.0.1'
-                            }
-                        }
-                    ]));
+            const versions = res.body.versions;
 
-                    done();
-                })
-                .catch(done.fail);
+            expect(versions).toEqual(jasmine.arrayContaining([
+                {
+                    api: 'api-package-1-namespace',
+                    apiDetails: {
+                        name: 'api-package-1',
+                        version: '0.0.1',
+                        description: 'Api package name space'
+                    }
+                },
+                {
+                    api: 'api-package-2',
+                    apiDetails: {
+                        name: 'api-package-2',
+                        version: '0.0.1'
+                    }
+                }
+            ]));
         });
 
-        it(`gets the metadata of an individual LabShare package`, done => {
-            request.get('/api-package-2/version')
-                .expect(200)
-                .then(res => {
-                    expect(res.body.version.api).toBe('api-package-2');
-                    expect(res.body.version.apiDetails.version).toBe('0.0.1');
-                    done();
-                })
-                .catch(done.fail);
+        it(`gets the metadata of an individual LabShare package`, async () => {
+            const res = await request.get('/api-package-2/version');
+
+            expect(res.statusCode).toBe(200);
+            expect(res.body.version).toBeDefined();
+            expect(res.body.version.api).toBe('api-package-2');
+            expect(res.body.version.apiDetails.version).toBe('0.0.1');
         });
 
-        it(`responds with a 404 when the LabShare package name does not exist`, done => {
-            request.post(`/api-package-1-namespace/version`).expect(404, done);
+        it(`responds with a 404 when the LabShare package name does not exist`, async () => {
+            await request.post(`/api-package-1-namespace/version`).expect(404);
         });
 
     });
