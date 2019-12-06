a4c868df141247bc46bcc7e5a5c9d7f73b82ba1b
Jamil Omar <jfalconi63@hotmail.com>
2019-12-06 17:23:19 -0500

    fix: support for lb4 services

--- cli/services.js
+++ cli/services.js
@@ -1,18 +1,27 @@
 'use strict';
 
-const Services = require('../lib/services'),
-    _ = require('lodash');
-
+const builders = require('../lib/build');
+const utils = require('../lib/utils');
+const  _ = require('lodash'); 
 exports.usage = [
     'lsc services start      - Start up LabShare API services.',
     ''
 ];
-
-exports.start = function () {
+exports.start = async function () {
     this.log.info('Starting LabShare services...');
-
-    let options = _.get(global.LabShare, 'Config.services'),
-        services = new Services(options);
-
-    services.start();
-};
+    debugger;
+    const labShareConfig = utils.LabShareConfig.loadLabShareConfig();
+    const lbOptions = require('config');
+    const mode  =  _.get(labShareConfig, 'mode' , 'services');
+    let builder;
+    switch(mode){
+        case 'services':
+                builder = new builders.ServicesBuilder(labShareConfig ,this.config , lbOptions);
+            break;
+        case 'lb':
+                builder = new builders.LbBuilder(labShareConfig , lbOptions);
+            break;
+    }
+    await builder.build();
+    return  
+  }
